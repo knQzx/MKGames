dc74d3f139020a39c0981c20832d3be2745864a2
knqzx <>
2021-11-30 22:53:28 +0300

    I made a database and a 'User' table, changing play to buy on the button. It is necessary to implement the launch of the game, checking if there is no "-", debiting a coin or refusal...

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
+import sqlite3
 
 from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 
 
 class MyWidget(QMainWindow):
@@ -11,18 +12,79 @@ class MyWidget(QMainWindow):
         self.pushButton.clicked.connect(self.run_s2adv)
         self.pushButton_2.clicked.connect(self.run_flappy_bird)
         self.pushButton_3.clicked.connect(self.run_snake)
+        """
+        we upload the user to the database 
+        if he is not there yet and change the coin
+        """
+        conn = sqlite3.connect("database.sqlite")
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * FROM User""")
+        if cursor.fetchone() is None:
+            cursor.execute("""INSERT INTO User
+                              VALUES ('1', '0', '0',
+                              '0', '-')"""
+                           )
+            conn.commit()
+        """
+        if there are no purchased games yet,
+        then we suggest buying one
+        """
+        cursor.execute("""SELECT * FROM User""")
+        if cursor.fetchone()[4] == '-':
+            self.pushButton.setText('BUY (1 coin)')
+            self.pushButton_2.setText('BUY (1 coin)')
+            self.pushButton_3.setText('BUY (1 coin)')
 
     def run_s2adv(self):
-        # TODO: run game
-        pass
+        conn = sqlite3.connect("database.sqlite")
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * FROM User""")
+        if cursor.fetchone()[4] == '-':
+            cursor.execute(f"""UPDATE User
+                              SET purchased_games = 'S2ADV'""")
+            conn.commit()
+            msg = QMessageBox()
+            msg.setWindowTitle("Выполнена покупка игры")
+            msg.setText("Выполнена покупка игры S2WAdventure")
+            msg.exec_()
+            self.pushButton.setText('PLAY')
+        else:
+            # TODO: starting the game, checking if there is no "-", debiting a coin or refusal
+            pass
 
     def run_flappy_bird(self):
-        # TODO: run game
-        pass
+        conn = sqlite3.connect("database.sqlite")
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * FROM User""")
+        if cursor.fetchone()[4] == '-':
+            cursor.execute(f"""UPDATE User
+                               SET purchased_games = 'Bird'""")
+            conn.commit()
+            msg = QMessageBox()
+            msg.setWindowTitle("Выполнена покупка игры")
+            msg.setText("Выполнена покупка игры Bird")
+            msg.exec_()
+            self.pushButton_2.setText('PLAY')
+        else:
+            # TODO: starting the game, checking if there is no "-", debiting a coin or refusal
+            pass
 
     def run_snake(self):
-        # TODO: run game
-        pass
+        conn = sqlite3.connect("database.sqlite")
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * FROM User""")
+        if cursor.fetchone()[4] == '-':
+            cursor.execute(f"""UPDATE User
+                               SET purchased_games = 'Snake'""")
+            conn.commit()
+            msg = QMessageBox()
+            msg.setWindowTitle("Выполнена покупка игры")
+            msg.setText("Выполнена покупка игры Snake")
+            msg.exec_()
+            self.pushButton_3.setText('PLAY')
+        else:
+            # TODO: starting the game, checking if there is no "-", debiting a coin or refusal
+            pass
 
 
 if __name__ == '__main__':
