e089cf0631eeaa24d7930041a2363cda29e5fa13
magprone <magpronem@gmail.com>
2021-12-24 11:14:03 +0300

    -game.py:  *Add Camera class

---
+++ games/S2WAdventure/game.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2) - CELL_SIZE // 2
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2) - CELL_SIZE // 2
+
+    def draw_group(self, group):
+        draw_group = pygame.sprite.Group()
+        for sprite in group:
+            if not (sprite.rect.x + sprite.rect.w <= 0 or sprite.rect.x >= width or
+                    sprite.rect.y + sprite.rect.h <= 0 or sprite.rect.y >= height):
+                draw_group.add(sprite)
+        draw_group.draw(screen)
