0a156a212dfdf9ccbe60e41360640170a5278f93
magprone <magpronem@gmail.com>
2021-12-24 12:08:59 +0300

    -game_screen.py:  *Add Window class -start_screen.py:  *Add Window class -main.py:  *Add Setup class

--- games/S2WAdventure/game_screen.py
+++ games/S2WAdventure/game_screen.py
@@ -14,10 +14,21 @@ class Camera:
         self.dx = -(target.rect.x + target.rect.w // 2 - width // 2) - CELL_SIZE // 2
         self.dy = -(target.rect.y + target.rect.h // 2 - height // 2) - CELL_SIZE // 2
 
-    def draw_group(self, group):
+    def draw_group(self, group, screen):
         draw_group = pygame.sprite.Group()
         for sprite in group:
             if not (sprite.rect.x + sprite.rect.w <= 0 or sprite.rect.x >= width or
                     sprite.rect.y + sprite.rect.h <= 0 or sprite.rect.y >= height):
                 draw_group.add(sprite)
         draw_group.draw(screen)
+
+
+class GameScreen:
+    def start(self, setup):
+        while True:
+            setup.screen.fill(pygame.Color('green'))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    setup.operations.terminate()
+            pygame.display.flip()
+            setup.clock.tick(setup.FPS)
---
+++ games/S2WAdventure/main.py
@@ -0,0 +1,30 @@
+import pygame
+import operations
+from start_screen import StartScreen
+from game_screen import GameScreen
+
+
+class Setup:
+    def __init__(self):
+        pygame.init()
+
+        self.size = self.width, self.height = 0, 0
+        self.screen = pygame.display.set_mode(self.size, pygame.FULLSCREEN)
+        self.size = self.width, self.height = pygame.display.get_window_size()
+        print(self.size)
+        pygame.display.set_caption('S2WAdventure')
+
+        self.clock = pygame.time.Clock()
+        self.FPS = 60
+
+        self.operations = operations
+        self.StartScreen = StartScreen
+        self.GameScreen = GameScreen
+
+        self.state = self.StartScreen()
+        while True:
+            self.state = self.state.start(self)
+
+
+if __name__ == '__main__':
+    Setup()
