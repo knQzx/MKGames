480de05b5da975beea983d25e1fc4e8358677b25
magprone <magpronem@gmail.com>
2021-12-25 21:15:30 +0300

    Add level load

--- games/S2WAdventure/operations.py
+++ games/S2WAdventure/operations.py
@@ -3,16 +3,6 @@ import sys
 import pygame
 
 
-def load_level(name):
-    filename = os.path.join('data', 'levels', name)
-    with open(filename, 'r') as map_file:
-        level_map = [line.strip() for line in map_file]
-
-    max_width = max(map(len, level_map))
-
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
 def load_image(name, colorkey=None):
     filename = os.path.join('data', 'images', name)
     if not os.path.isfile(filename):
@@ -27,6 +17,20 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+def draw_background(screen, image):
+    image_ratio = image.get_width() / image.get_height()
+    screen_ratio = screen.get_width() / screen.get_height()
+    if image_ratio > screen_ratio:
+        new_image_size = screen.get_height() * image_ratio, screen.get_height()
+    else:
+        new_image_size = screen.get_width(), 1 / image_ratio / image.get_width()
+    image = pygame.transform.scale(image, new_image_size)
+    rect = image.get_rect()
+    rect.x, rect.y = screen.get_width() // 2 - image.get_width() // 2,\
+        screen.get_height() // 2 - image.get_height() // 2
+    screen.blit(image, rect)
 
 
 def terminate():
