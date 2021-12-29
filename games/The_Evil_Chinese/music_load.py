3e656c6a31a64990646104b1329dfb6b453da953
knqzx <>
2021-12-29 22:48:27 +0300

    я короче сделал чтобы он собирал рис, есть счётчик, и сделал чтобы рис убирался с карты. TODO: 1) стартовое окно 2) обнуление из-за выхода за черты 3) атака негров и то что они забирают рис 4) прохождение на следующий этап 5) экран победы

--- games/The_Evil_Chinese/music_load.py
+++ games/The_Evil_Chinese/music_load.py
@@ -18,3 +18,13 @@ class Music:
         pygame.mixer.music.load(f"data/musics/{music}")
         pygame.mixer.music.play(-1)
 
+    def pause(self):
+        pygame.mixer.music.pause()
+
+    def unpause(self):
+        pygame.mixer.music.unpause()
+
+    def nyam_nyam(self):
+        song = pygame.mixer.Sound('data/musics/Скушал рис.mp3')
+        song.play()
+
--- games/The_Evil_Chinese/start_screen.py
+++ games/The_Evil_Chinese/start_screen.py
@@ -33,24 +33,55 @@ class Player:
 
 if __name__ == '__main__':
     pygame.init()
+    pygame.font.init()
     pygame.display.set_caption('The Evil Chinese v.1.0.0')
     size = width, height = 800, 800
+    sxr = 30
     screen = pygame.display.set_mode(size)
     # обещание великого правителя
-    Music().rulers_promise('Обещание выдать миску риса.mp3')
+    #Music().rulers_promise('Обещание выдать миску риса.mp3')
     # музыка на фоне
     Music().background_music('Музыка на фон.mp3')
+    #
+    k = 0
+    rise_x_y = {}
+    for el in open('data/levels/level_1/level_1').readlines():
+        playeer = Player().player_load(el)  # получаем список координат игрока
+        walls = LoadGame().walls_load(el)  # получаем список координат стен
+        floar = LoadGame().floor_load(el)  # получаем список координат пола
+        rise = Rise().rise_load(el)  # получаем список координат риса
+        if rise:
+            new_rise = [el1 * 10 for el1 in rise]
+            rise_x_y[k] = new_rise
+        if playeer:  # --> если не пустой
+            # загружаем картинку нашего злобного китайца
+            player = pygame.image.load('data/images/chinese.png').convert_alpha()
+            player = pygame.transform.scale(player, (25, 25))
+            x_pos = playeer[0] * 9.6
+            y_pos = k - 6
+        k += 20
+    print(rise_x_y)
+    #
     running = True
     clock = pygame.time.Clock()
+    v = 25  # пикселей в секунду
+    # наш счет
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface = myfont.render('Количество риса: 0', False, 'blue')
+    screen.blit(textsurface, (0, 0))
+    rise_result = 0
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         k = 0
+        screen.fill((0, 0, 0))
+        textsurface = myfont.render(f'Количество риса: {rise_result}', False, 'blue')
+        screen.blit(textsurface, (600, 0))
         for el in open('data/levels/level_1/level_1').readlines():
             walls = LoadGame().walls_load(el)  # получаем список координат стен
-            floar =  LoadGame().floor_load(el)  # получаем список координат пола
-            playeer = Player().player_load(el)  # получаем список координат игрока
+            floar = LoadGame().floor_load(el)  # получаем список координат пола
             rise = Rise().rise_load(el)  # получаем список координат риса
             if walls:  # --> если не пустой
                 for el1 in walls:  # проходимся по элементам т.к. может быть не одна стена
@@ -60,15 +91,37 @@ if __name__ == '__main__':
                 for el1 in floar:  # проходимся по элементам т.к. может быть не один пол
                     el1 *= 10  # подгоняем под размеры
                     pygame.draw.line(screen, 'red', (el1 - 10, k), (el1 + 10, k))
-            if playeer:  # --> если не пустой
-                # загружаем картинку нашего злобного китайца
-                player = pygame.image.load('data/images/chinese.png').convert_alpha()
-                player = pygame.transform.scale(player, (25, 25))
-                screen.blit(player, (playeer[0] * 9.6, k - 6))
-            if rise:  # --> если не пустой
-                for el1 in rise:  # проходимся по элементам т.к. может быть не один рис
-                    el1 *= 10  # подгоняем под размеры
-                    pygame.draw.rect(screen, 'green', (el1, k, 5, 5))  # рисуем зеленый квадрат(рис)
+            if rise_x_y:  # рисуем зеленый квадрат(рис)
+                for y in rise_x_y:
+                    for el in rise_x_y[y]:
+                        pygame.draw.rect(screen, 'green', (int(el), int(y), 5, 5))
             k += 20
-            pygame.display.flip()
+        screen.blit(player, (x_pos, y_pos))
+        key = pygame.key.get_pressed()
+        if key[pygame.K_d]:
+            x_pos += v * clock.tick() / 1000
+        elif key[pygame.K_a]:
+            x_pos -= v * clock.tick() / 1000
+        elif key[pygame.K_w]:
+            y_pos -= v * clock.tick() / 1000
+        elif key[pygame.K_s]:
+            y_pos += v * clock.tick() / 1000
+        # проверка на то попал ли игрок в рис, если да - удаляем из словаря этот рис
+        # простым языком проверка на то коснулся ли он риса, если да - удаляем с карты
+        # {180: [50], 280: [50, 90], 380: [150, 260], 600: [150, 220]}
+        for el in rise_x_y:
+            if el - 20 <= y_pos <= el:
+                for qw in rise_x_y[el]:
+                    if qw - 20 <= x_pos <= qw:
+                        rise_x_y[el].remove(qw)
+                        Music().pause()
+                        Music().nyam_nyam()
+                        # НЕ БАГ А ФИЧА !!!!!!!!!!!!!
+                        for i in range(1000):
+                            s = 100 - 12 * 12 + 3
+                        Music().background_music('Музыка на фон.mp3')
+                        rise_result += 1
+                        textsurface = myfont.render(f'Количество риса: {rise_result}', False, 'blue')
+                        screen.blit(textsurface, (600, 0))
+        pygame.display.flip()
     pygame.quit()
