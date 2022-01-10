cd81b43a1d6cf3eef482a0469df002d029dc9ec0
knqzx <>
2022-01-10 18:58:05 +0300

    1) fixed the screen resolution for the S2WAdventure game (as in BeerFlight) 2) finished the qt.app, names and database 3) changed the file name (start_screen.py to main.py )

--- games/BeerFlight/main.py
+++ games/BeerFlight/main.py
@@ -8,17 +8,11 @@ from level_screen import LevelScreen
 class Setup:
     def __init__(self):
         pygame.init()
-
         display_info = pygame.display.Info()
         if display_info.current_w >= 2500:
             self.size = self.width, self.height = display_info.current_w - 1000, display_info.current_h - 80
-            print(display_info.current_w)
-            print(1)
         else:
-            print(display_info.current_w)
-            print(2)
             self.size = self.width, self.height = display_info.current_w, display_info.current_h
-        print(pygame.FULLSCREEN)
         self.screen = pygame.display.set_mode(self.size, pygame.FULLSCREEN)
         self.size = self.width, self.height = pygame.display.get_window_size()
         pygame.display.set_caption('BeerFlight')
