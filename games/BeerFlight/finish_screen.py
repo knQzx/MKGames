37b0342f44f7ed390f55416e6cedfe206e1f03cd
knqzx <>
2022-01-12 23:28:22 +0300

    помогите, меня держат в заложниках, я целый час писал что делает каждая строка кода(((

--- games/BeerFlight/finish_screen.py
+++ games/BeerFlight/finish_screen.py
@@ -3,30 +3,39 @@ import operations
 
 
 class FinishScreen:
-    def __init__(self, level_name: str, win: bool, stars: int):
+    def __init__(self, level_name: str, win: bool, stars: int):  # --> set default parameters
         self.level_name = level_name
         self.win = win
         self.stars = stars
 
-    def draw(self):
+    def draw(self):  # --> make function draw
         font = pygame.font.Font(None, 100)  # Draw win state
         font.set_bold(True)
+        # check self.win, if true
         if self.win:
             text_str = 'Completed'
+        # if false
         else:
             text_str = 'Lose'
+        # render font parameters
         text = font.render(text_str, True, pygame.Color('yellow'))
         text_rect = text.get_rect()
+        # get width for x parameter
         text_rect.x = self.image.get_width() // 2 - text.get_width() // 2
+        # get get screen coords for y parameter
         text_rect.y = operations.get_screen_coords(self.image, (0, 0.25))[1]
         self.image.blit(text, text_rect)
 
         indent = 30  # Draw stars
+        # make parameter count for the stars
         count = self.stars
+        # load start image
         star_image = pygame.transform.scale(operations.load_image('Star.png'), (145, 145))
         star_image_rect = star_image.get_rect()
+        # get width for x parameter
         star_image_rect.x = self.image.get_width() // 2 - (
                 star_image.get_width() * count + indent * (count - 1)) // 2
+        # get get screen coords for y parameter
         star_image_rect.y = operations.get_screen_coords(self.image, (0, 0.4))[1]
         for _ in range(count):
             self.image.blit(
@@ -35,57 +44,67 @@ class FinishScreen:
             )
             star_image_rect.x += star_image.get_width() + indent
 
-    def set_buttons(self):
+    def set_buttons(self):  # --> in this function, we directly make buttons
         play_again_image = pygame.transform.scale(operations.load_image('Play again.png'), (100, 100))  # Place play again button
+        # get width for x parameter
         new_x = int(self.image.get_width() / 2 + play_again_image.get_rect().width / 2 * 1.5)
+        # get height for y parameter
         new_y = self.image.get_height() // 2 + play_again_image.get_rect().height
+        # initialization "play_again" buttons
         self.play_again_button = Button(
             play_again_image,
             (new_x, new_y)
         )
+        # add buttons to group
         self.buttons_group.add(self.play_again_button)
-
+        # initialization image for select level
         select_level_image = pygame.transform.scale(operations.load_image('Select level.png'), (100, 100))  # Place select level button
+        # get width for x parameter
         new_x = int(self.image.get_width() / 2 - select_level_image.get_rect().width * 1.5)
+        # get height for y parameter
         new_y = self.image.get_height() // 2 + select_level_image.get_rect().height
+        # initialization "select_level" buttons
         self.select_level_button = Button(
             select_level_image,
             (new_x, new_y)
         )
+        # add buttons to group
         self.buttons_group.add(self.select_level_button)
 
-    def check_click(self, pos):
+    def check_click(self, pos):  # --> in this function, we directly check the user's click
         if self.select_level_button.rect.collidepoint(*pos):
             self.screen_out = self.setup.LevelScreen()
         if self.play_again_button.rect.collidepoint(*pos):
             self.screen_out = self.setup.GameScreen(self.level_name)
 
-    def set_music(self):
+    def set_music(self):  # --> in this function, we directly set win or lose musics
         if self.win:
             pygame.mixer.music.load(f'data/music/Win.mp3')
         else:
             pygame.mixer.music.load(f'data/music/Lose.mp3')
         pygame.mixer.music.play(1)
 
-    def start(self, setup):
+    def start(self, setup):  # --> this is our start function
         self.setup = setup
-
+        # set music
         self.set_music()
 
         self.image = pygame.Surface(self.setup.screen.get_size(), pygame.SRCALPHA, 32)
         self.draw()
-
+        # initialization buttons group
         self.buttons_group = pygame.sprite.Group()
         self.set_buttons()
-
+        # set default screen_out parameter as None
         self.screen_out = None
         while True:
+            # screen fill to blue
             self.setup.screen.fill(pygame.Color('blue'))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     setup.operations.terminate()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     self.check_click(event.pos)
+            # check screen_out
             if self.screen_out is not None:
                 pygame.mixer.music.pause()
                 return self.screen_out
@@ -96,7 +115,7 @@ class FinishScreen:
 
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, image, pos):
+    def __init__(self, image, pos):  # --> set default parameters
         super().__init__()
         self.image = image
         self.pos = pos
--- games/BeerFlight/game_screen.py
+++ games/BeerFlight/game_screen.py
@@ -8,18 +8,22 @@ import operations
 
 class Camera:  # Camera whose apply objects with main sprite
     def __init__(self):
+        # self.setup.FinishScreen(self.name, False, 3)
         self.dx = 0
         self.dy = 0
 
     def apply(self, obj):
+        # adding the dx parameter to the x parameter
         obj.rect.x += self.dx
 
     def update(self, target, game_screen):
+        # обновляем наш параметр dx
         self.dx = -(target.rect.x + target.rect.w // 2 - game_screen.setup.width // 4)
         target.rect.x += self.dx
         target.x += self.dx
 
     def draw_group(self, group, screen):
+        # draw a group of sprites
         draw_group = pygame.sprite.Group()
         for sprite in group:
             if not (sprite.rect.x + sprite.rect.w <= 0 or sprite.rect.x >= screen.get_width() or
@@ -31,10 +35,12 @@ class Camera:  # Camera whose apply objects with main sprite
 class Hero(pygame.sprite.Sprite):  # Sprite of main hero
     def __init__(self, x, y, game_screen):
         super().__init__()
+        # we set the default values
         self.game_screen = game_screen
         self.ticks_to_change = 10
         self.sheet_state = 0
         self.cur_frame = 0
+        # uploading images
         self.sheets = [
             self.cut_sheet(operations.load_image('Walking_hero_sheet5x2.png'), 5, 2),
             self.cut_sheet(operations.load_image('Flying_hero_sheet1x1.png'), 1, 1)
@@ -81,8 +87,10 @@ class GameScreen:  # Screen for game at any level
         self.name = name
 
     def load_level(self):  # Load selected level
+        # open our json layer and load it
         with open(f'data/levels/{self.name}/level.json') as read_file:
             self.level = json.load(read_file)
+        # loading parameters
         self.map = pytmx.load_pygame(f'data/levels/{self.name}/level.tmx')
         self.height = self.map.height
         self.width = self.map.width
@@ -91,8 +99,9 @@ class GameScreen:  # Screen for game at any level
         self.boss_triggers = [8]
         self.death_tiles = [4, 9, 14, 11, 12, 13]
         self.end_tiles = [9, 15]
-
+        # turn on the music
         pygame.mixer.music.load(f'data/music/{self.level["music"]}')
+        # making music endless
         pygame.mixer.music.play(-1)
 
     def set_tiles_and_triggers(self):  # Set tiles and triggers at field
@@ -102,6 +111,7 @@ class GameScreen:  # Screen for game at any level
                 image = self.map.get_tile_image(x, y, 0)
                 if image is None:
                     continue
+                # setting parameters for tiles
                 tile.image = pygame.transform.scale(image, (self.tile_size, self.tile_size))
                 tile.mask = pygame.mask.from_surface(tile.image)
                 tile.rect = tile.image.get_rect()
@@ -116,15 +126,19 @@ class GameScreen:  # Screen for game at any level
                 if tile_id in self.end_tiles:
                     self.end_tiles_group.add(tile)
                 self.tiles_group.add(tile)
-
+                # specifying triggers
                 trigger = pygame.sprite.Sprite()  # Set triggers
                 image = self.map.get_tile_image(x, y, 0)
                 if image is None:
                     continue
+                # specifying triggers image
                 trigger.image = pygame.transform.scale(image, (self.tile_size, self.tile_size))
+                # specifying triggers mask
                 trigger.mask = pygame.mask.from_surface(tile.image)
+                # specifying triggers rect
                 trigger.rect = tile.image.get_rect()
                 trigger.rect.x, tile.rect.y = x * self.tile_size, y * self.tile_size
+                # specifying triggers trigger_id
                 trigger_id = self.get_tile_id((x, y), 0)
                 if trigger_id in self.boss_triggers:
                     self.triggers_group.add(trigger)
@@ -136,15 +150,24 @@ class GameScreen:  # Screen for game at any level
         for el in self.death_tiles_group:
             if pygame.sprite.collide_mask(hero, el):
                 self.running = False
+                self.you_win = False
                 break
 
-    def start(self, setup):
-        self.setup = setup
+    def check_end(self, hero):  # --> check collision with end tiles group
+        for el in self.end_tiles_group:
+            if pygame.sprite.collide_mask(hero, el):
+                self.running = False
+                self.you_win = True
+                break
 
+    def start(self, setup):  # --> function to start the game
+        self.setup = setup
+        # load level
         self.load_level()
+        # set size
         self.tile_size = self.setup.height // self.map.height
         self.PPM = self.tile_size / 2
-
+        # set different sprites groups
         self.tiles_group = pygame.sprite.Group()
         self.default_tiles_group = pygame.sprite.Group()
         self.stars_tiles_group = pygame.sprite.Group()
@@ -152,21 +175,30 @@ class GameScreen:  # Screen for game at any level
         self.death_tiles_group = pygame.sprite.Group()
         self.end_tiles_group = pygame.sprite.Group()
         self.set_tiles_and_triggers()
-
+        # initialization camera
         camera = Camera()
-
+        # initialization player
         hero = Hero(0, self.map.height - 1, self)
+        # add player to hero_group
         hero_group = pygame.sprite.Group()
         hero_group.add(hero)
-
+        # load background image
         background = operations.load_image(self.level['background'])
         space_clicked = pygame.key.get_pressed()[pygame.K_SPACE]
+        # set parameter self.running as True
         self.running = True
+        # set parameter self.you_win as True
+        self.you_win = None
         while True:
+            # draw background
             operations.draw_background(self.setup.screen, background)
+            # check parameter self.running
             if not self.running:
                 pygame.mixer.music.pause()
                 return self.setup.FinishScreen(self.name, False, 3)
+            # check parameter self.you_win
+            if self.you_win:
+                return self.setup.FinishScreen(self.name, True, 3)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     setup.operations.terminate()
@@ -179,13 +211,18 @@ class GameScreen:  # Screen for game at any level
                 hero.dy -= self.PPM * 9 / self.setup.FPS
                 hero.sheet_state = 1
             hero.update()
+            # update camera
             camera.update(hero, self)
             for tile in self.tiles_group:
                 camera.apply(tile)
             for trigger in self.triggers_group:
                 camera.apply(trigger)
+            # draw tiles
             camera.draw_group(self.tiles_group, self.setup.screen)
             hero_group.draw(self.setup.screen)
+            # check collisions with lasers
             self.check_lasers(hero)
+            # check collisions with end_tiles_group
+            self.check_end(hero)
             pygame.display.flip()
             setup.clock.tick(setup.FPS)
